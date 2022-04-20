bec937e5765f16bb5e3fb123227a93671054789d
manu567s <104025697+manu567s@users.noreply.github.com>
2022-04-20 15:10:34 +0530

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Get button by ID
-  var button = document.getElementById('envato');
+  var button = document.getElementById('spotify');
   button.onclick = () => {
     chrome.tabs.create(
-      { url: `https://elements.envato.com/sign-in` }
+      { url: `https://accounts.spotify.com/` }
     )
   }
 });
